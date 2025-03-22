10367bdac0546caad231cce1aac62b1e3d3b0c8a
Gabbar <gabbar-v7@proton.me>
2025-03-22 23:33:32 +0530

    tasks ui

--- lib/visuals/screens/tasks_page.dart
+++ lib/visuals/screens/tasks_page.dart
@@ -9,8 +9,64 @@ class TasksPage extends StatefulWidget {
 }
 
 class _TasksPageState extends State<TasksPage> {
+  Widget _taskTile() {
+    return GestureDetector(
+      onTap: () {},
+      onHorizontalDragEnd: (details) {
+        if (details.primaryVelocity! > 0) {
+          print("Right");
+        } else if (details.primaryVelocity! < 0) {
+          print("Left");
+        }
+      },
+      child: ListTile(
+        leading: Checkbox(value: false, onChanged: (value) {}),
+        title: Text("Hello World!"),
+        trailing: Text("21/02"),
+      ),
+    );
+  }
+
+  Widget pageBody() {
+    return ListView.separated(
+      itemCount: 20, // Number of items
+      itemBuilder: (context, index) {
+        return _taskTile();
+      },
+      separatorBuilder: (context, index) {
+        return Divider(indent: 20, endIndent: 20); // Custom separator
+      },
+    );
+  }
+
+  void _showModalBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      constraints: BoxConstraints(maxWidth: double.infinity),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(25)),
+      ),
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(10),
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            child: Text('asd'),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppStyles.appBar("ToDo List", context));
+    return Scaffold(
+      appBar: AppStyles.appBar("ToDo List", context),
+      body: pageBody(),
+      floatingActionButton: AppStyles.floatingActionButton(Icons.add, () {
+        _showModalBottomSheet();
+      }),
+    );
   }
 }
