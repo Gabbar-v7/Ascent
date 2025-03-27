b6b6c0fca1dbdaba131c32792ad7f49babf62f75
Gabbar <gabbar-v7@proton.me>
2025-03-27 22:44:24 +0530

    gradle update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 android {
     namespace = "com.example.ascent"
     compileSdk = flutter.compileSdkVersion              // 35
-    ndkVersion = flutter.ndkVersion                     // 26.3.11579264
     ndkVersion = "27.0.12077973"
 
     compileOptions {
