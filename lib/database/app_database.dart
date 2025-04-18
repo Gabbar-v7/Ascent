25b456e672faada0189f60c3500c5274042a5649
Gabbar-v7 <gabbar-v7@proton.me>
2025-04-18 23:53:59 +0530

    db file location update

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,6 +1,7 @@
 import 'package:drift/drift.dart';
 import 'package:ascent/database/tables/tasks_table.dart';
 import 'package:drift_flutter/drift_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 
 part 'app_database.g.dart';
 
@@ -12,6 +13,14 @@ class AppDatabase extends _$AppDatabase {
   int get schemaVersion => 1;
 
   static QueryExecutor _openConnection() {
-    return driftDatabase(name: 'database');
+    return driftDatabase(
+      name: 'database',
+      native: const DriftNativeOptions(
+        // By default, `driftDatabase` from `package:drift_flutter` stores the
+        // database files in `getApplicationDocumentsDirectory()`.
+        databaseDirectory: getApplicationDocumentsDirectory,
+      ),
+      // If you need web support, see https://drift.simonbinder.eu/platforms/web/
+    );
   }
 }
