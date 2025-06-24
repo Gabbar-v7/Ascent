3e058472417468ad361326c7ca19981b1da71c55
Gabbar <gabbar-v7@proton.me>
2025-06-24 17:41:34 +0530

    launch image night, app rename, app package rename

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,22 +6,22 @@ plugins {
 }
 
 android {
-    namespace = "com.example.ascent"
+    namespace = "com.value.ascent"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ascent"
+        applicationId = "com.value.ascent"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -30,10 +30,28 @@ android {
         versionName = flutter.versionName
     }
 
+     signingConfigs {
+        create("release") {
+            if (System.getenv("KEYSTORE_FILE") != null) {
+                storeFile = file(System.getenv("KEYSTORE_FILE"))
+                storePassword = System.getenv("STORE_PASSWORD")
+                keyAlias = System.getenv("KEY_ALIAS")
+                keyPassword = System.getenv("KEY_PASSWORD")
+            }        
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("release")
+        }
+
+        debug {
+            applicationIdSuffix = ".debug"
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- android/app/src/main/kotlin/com/value/ascent/MainActivity.kt
+++ android/app/src/main/kotlin/com/value/ascent/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.ascent
+package com.value.ascent
 
 import io.flutter.embedding.android.FlutterActivity
 
