61dc5038ae3aa5a473874ca180e19ee0370b6b63
Gabbar <gabbar-v7@proton.me>
2025-07-19 20:29:41 +0530

    --01--

--- lib/visuals/home/notes/notes_index.dart
+++ lib/visuals/home/notes/notes_index.dart
@@ -1,9 +1,4 @@
-import 'package:ascent/database/app_database.dart';
-import 'package:ascent/services/drift_service.dart';
-import 'package:ascent/visuals/home/notes/widgets.dart';
-import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 
 class NotesIndex extends StatefulWidget {
   const NotesIndex({super.key});
@@ -13,42 +8,8 @@ class NotesIndex extends StatefulWidget {
 }
 
 class _NotesIndexState extends State<NotesIndex> {
-  final database = DriftService.instance.driftDB;
-  List<Note> notes = [];
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchNotes();
-  }
-
-  void _fetchNotes() async {
-    notes = await (database.select(database.notes)
-          ..where((tbl) =>
-              tbl.archived.isNotValue(true) & tbl.trashed.isNotValue(true))
-          ..orderBy([
-            (tbl) =>
-                OrderingTerm(expression: tbl.updatedAt, mode: OrderingMode.desc)
-          ]))
-        .get();
-    setState(() {
-      notes;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        itemBuilder: _noteTileBuilder,
-        separatorBuilder: _sepratorBuilder,
-        itemCount: notes.length);
-  }
-
-  Widget _noteTileBuilder(BuildContext context, int index) {
-    return noteTilePlainText(context, notes[index]);
-  }
-
-  Widget _sepratorBuilder(BuildContext context, int index) {
-    return const Gap(5);
+    return Column();
   }
 }
